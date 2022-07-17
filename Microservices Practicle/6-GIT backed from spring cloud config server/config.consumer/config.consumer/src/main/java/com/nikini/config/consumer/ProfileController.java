2cccb82c2b8bcbe495225bc8e3221ecea1246ce8
NikiniJayasekara <102573457+NikiniJayasekara@users.noreply.github.com>
2022-07-17 23:57:51 +0530

    Add files via upload

---
+++ Microservices Practicle/6-GIT backed from spring cloud config server/config.consumer/config.consumer/src/main/java/com/nikini/config/consumer/ProfileController.java
@@ -0,0 +1,30 @@
+package com.nikini.config.consumer;
+
+
+import com.nikini.config.consumer.model.MemberProfileConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProfileController {
+
+
+    @Autowired
+    MemberProfileConfiguration memberProfileConfiguration;
+
+
+    @RequestMapping("/profile")
+    public String getConfig(Model model){
+
+        model.addAttribute( "model",memberProfileConfiguration.getDefaultModel());
+        model.addAttribute( "min",memberProfileConfiguration.getMinRentPeriod());
+        return "mprofile";
+
+    }
+
+}
+
+
+
