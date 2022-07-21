9e9e824bd1750532ac9d47c38511cf390d1ac96d
NikiniJayasekara <102573457+NikiniJayasekara@users.noreply.github.com>
2022-07-21 16:34:22 +0530

    Add files via upload

---
+++ Microservices Practicle/13/rent-ui/rent-ui/profile-service/src/main/java/com/nikini/rentcloud/profileservice/service/CustomerService.java
@@ -0,0 +1,15 @@
+package com.nikini.rentcloud.profileservice.service;
+
+import com.micro.rentcloud.commons.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+
+    Customer save(Customer customer);
+
+    Customer fetchById(int profileId);
+
+    List<Customer> fetchAllProfiles();
+}
