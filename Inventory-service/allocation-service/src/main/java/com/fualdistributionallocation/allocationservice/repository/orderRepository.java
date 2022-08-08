a3b80b726016ed64b3a0a1cec0eb1e739c66f757
NikiniJayasekara <102573457+NikiniJayasekara@users.noreply.github.com>
2022-08-08 22:44:47 +0530

    Delete Inventory-service/allocation-service directory

--- Inventory-service/allocation-service/src/main/java/com/fualdistributionallocation/allocationservice/repository/orderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fualdistributionallocation.allocationservice.repository;
-
-import com.fualdistributionallocation.allocationservice.model.Order;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface orderRepository extends MongoRepository<Order, Integer> {
-}
